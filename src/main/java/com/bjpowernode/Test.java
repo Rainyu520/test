25bcc4197232b4172a9af8d92f3fc4153c6c3319
y8705 <870538914@qq.com>
2019-10-17 10:36:20 +0800

    new

---
+++ src/main/java/com/bjpowernode/Test.java
@@ -0,0 +1,23 @@
+package com.bjpowernode;/**
+ * ClassName:Test
+ * Package:com.bjpowernode
+ * Description
+ *
+ * @date ：${Date}
+ */
+
+
+
+/**
+ *Author：Rainyu
+ *2019/10/15
+ */
+
+public class Test {
+    public static void main(String[] args) {
+       Integer a  =-128;
+       Integer b =-128;
+        System.out.println (a==b);
+
+    }
+}
