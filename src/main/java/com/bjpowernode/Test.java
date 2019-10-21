f3d92427dc9e684ffad48687037e8ee61d6b3300
y8705 <870538914@qq.com>
2019-10-21 21:40:52 +0800

    String

--- src/main/java/com/bjpowernode/Test.java
+++ src/main/java/com/bjpowernode/Test.java
@@ -16,7 +16,7 @@ public class Test {
         System.out.println ("code2测试");
         System.out.println ("test2测试");
         System.out.println ("test3测试");
-        String x= "3";
+        String y= "3";
 
         int x= 6;
 
