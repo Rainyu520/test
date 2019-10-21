b685c172a9bcea457202afcf71ff8863119e4b46
y8705 <870538914@qq.com>
2019-10-21 21:51:27 +0800

    w

--- src/main/java/com/bjpowernode/Test.java
+++ src/main/java/com/bjpowernode/Test.java
@@ -15,11 +15,6 @@ public class Test {
 
         System.out.println ("哈哈");
 
-        System.out.println ("test测试");
-        System.out.println ("code3测试");
-        System.out.println ("code2测试");
-        System.out.println ("test2测试");
-        System.out.println ("test3测试");
         String y= "3";
 
         int x= 6;
